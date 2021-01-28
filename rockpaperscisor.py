3e81ae8cd67e599ce1934f209b6cf6cf3f9c4b2c
sandetyagi0831 <77581211+sandetyagi0831@users.noreply.github.com>
2021-01-28 22:57:42 +0530

    Add files via upload

---
+++ rockpaperscisor.py
@@ -0,0 +1,22 @@
+# creating a game of rock paper scissor
+
+import random
+
+def play():
+    user = input("enter 'r' for rock, 'p' paper and 's' for scissor\n")
+    comp = random.choice(['r','p','s'])
+
+    if user==comp:
+        print("its a Tie")
+
+    if iswin(user,comp):
+        return 'You Won!!'
+
+    return 'You Lost!!'
+
+def iswin(player,opponent):
+    if (player=='r' and opponent=='s') or (player=='p' and opponent=='r') or (player=='s' and opponent=='p'):
+        return True
+print(play())
+
+
